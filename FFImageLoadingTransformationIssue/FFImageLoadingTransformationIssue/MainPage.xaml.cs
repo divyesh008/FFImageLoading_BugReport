ba5cccad1fcf14802cc66beb6b6a947195960ba1
chetan <chetans@differenzsystem.com>
2019-12-04 19:56:48 +0530

    used Xamarin.Plugin.ImageEdit

--- FFImageLoadingTransformationIssue/FFImageLoadingTransformationIssue/MainPage.xaml.cs
+++ FFImageLoadingTransformationIssue/FFImageLoadingTransformationIssue/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,9 +15,39 @@ namespace FFImageLoadingTransformationIssue
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+      
         public MainPage()
         {
             InitializeComponent();
+
+            ResizePic.Clicked += ResizePic_Clicked;
+            Monochrome.Clicked += Monochrome_Clicked;
+        }
+
+        private async void Monochrome_Clicked(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            byte[] imageBytes = webClient.DownloadData("https://tinyurl.com/qph49y4");
+
+            using (var image = await Plugin.ImageEdit.CrossImageEdit.Current.CreateImageAsync(imageBytes))
+            {
+                var data = image.ToMonochrome().ToJpeg(50);
+                cacheImg.Source = ImageSource.FromStream(() => new MemoryStream(data));
+                CircleImg.Source = ImageSource.FromStream(() => new MemoryStream(data));
+            }
+        }
+
+        private async void ResizePic_Clicked(object sender, EventArgs e)
+        {
+            WebClient webClient = new WebClient();
+            byte[] imageBytes = webClient.DownloadData("https://tinyurl.com/qph49y4");
+
+            using (var image = await Plugin.ImageEdit.CrossImageEdit.Current.CreateImageAsync(imageBytes))
+            {
+                var data = image.Crop(0, 1, 100, 100).ToPng();
+                cacheImg.Source = ImageSource.FromStream(() => new MemoryStream(data));
+                CircleImg.Source = ImageSource.FromStream(() => new MemoryStream(data));
+            }
         }
     }
 }
